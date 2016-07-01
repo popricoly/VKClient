6e83779860d453151fe0bd87340c2552f3027f5c
Sasha Matviichuk <matviichuk@dstudiosoft.com>
2016-07-02 01:43:39 +0300

    project initialise commit

---
+++ VKClient/Pods/Target Support Files/Pods-VKClient-VKClientTests/Pods-VKClient-VKClientTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VKClient_VKClientTests : NSObject
+@end
+@implementation PodsDummy_Pods_VKClient_VKClientTests
+@end
---
+++ VKClient/Pods/Target Support Files/Pods-VKClient-VKClientUITests/Pods-VKClient-VKClientUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VKClient_VKClientUITests : NSObject
+@end
+@implementation PodsDummy_Pods_VKClient_VKClientUITests
+@end
